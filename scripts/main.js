f035dc5e8c3817e475518f3006655bc77626420e
maidan-next <maidan-next@users.noreply.github.com>
2014-01-08 02:41:00 +0200

    move to json

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,22 @@
 (function(document, undefined) {
   document.onreadystatechange = function() {
     if (document.readyState === "complete") {
-      var sectionsTemplate = document.getElementById('sections-template'),
-          dataContainer = document.getElementById('data-container'),
-          template = Hogan.compile(sectionsTemplate.innerHTML);
+      // Query data
+      reqwest({
+        'url': 'data.json',
+        'type': 'json',
+        'error': function() {
+          console.error('Unable to get or parse data');
+        },
+        'success': function(data) {
+          // Render data     
+          var sectionsTemplate = document.getElementById('sections-template'),
+              dataContainer = document.getElementById('data-container'),
+              template = Hogan.compile(sectionsTemplate.innerHTML);
 
-      dataContainer.innerHTML = template.render(data);
+          dataContainer.innerHTML = template.render(data);
+        }
+      });
     }
   }
 })(document);
