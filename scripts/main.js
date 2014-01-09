fedbd50105a32c08e47f9bc57697ec3ae3d91321
maidan-next <maidan-next@users.noreply.github.com>
2014-01-09 14:09:11 +0200

    moved to jquery

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,43 @@
-(function(document, undefined) {
-  document.onreadystatechange = function() {
-    if (document.readyState === "complete") {
-      // Query data
-      reqwest({
-        'url': 'data.json',
-        'type': 'json',
-        'error': function() {
-          console.error('Unable to get or parse data');
-        },
-        'success': function(data) {
-          // Render data     
-          var sectionsTemplate = document.getElementById('sections-template'),
-              dataContainer = document.getElementById('data-container'),
-              template = Hogan.compile(sectionsTemplate.innerHTML);
+;(function(document, undefined) {
+  var DATA_URL = 'data.json';
 
-          dataContainer.innerHTML = template.render(data);
-        }
+  $(function() {
+    // Query data
+    $.getJSON(DATA_URL)
+      .done(function(data) {
+        // Render data     
+        var $sectionsTemplate = $('#sections-template'),
+            $dataContainer = $('#data-container'),
+            template = Hogan.compile($sectionsTemplate.html());
+
+        $dataContainer.html(template.render(data));
+      })
+      .error(function() {
+        console.error('Unable to get or parse data');
       });
 
-      // Social buttons
-      (function(doc, script) {
-        var js,
-            fjs = doc.getElementsByTagName(script)[0],
-            frag = doc.createDocumentFragment(),
-            add = function(url, id) {
-                if (doc.getElementById(id)) {
-                    return;
-                }
-                js = doc.createElement(script);
-                js.src = url;
-                id && (js.id = id);
-                frag.appendChild(js);
-            };
+    // Social buttons
+    (function(doc, script) {
+      var js,
+          fjs = doc.getElementsByTagName(script)[0],
+          frag = doc.createDocumentFragment(),
+          add = function(url, id) {
+              if (doc.getElementById(id)) {
+                  return;
+              }
+              js = doc.createElement(script);
+              js.src = url;
+              id && (js.id = id);
+              frag.appendChild(js);
+          };
 
-        // Google+ button
-        add('http://apis.google.com/js/plusone.js');
-        // Facebook SDK
-        add('//connect.facebook.net/uk_UA/all.js#xfbml=1', 'facebook-jssdk');
-        // Twitter SDK
-        add('//platform.twitter.com/widgets.js');
-        fjs.parentNode.insertBefore(frag, fjs);
-      }(document, 'script'));
-    }
-  }
+      // Google+ button
+      add('http://apis.google.com/js/plusone.js');
+      // Facebook SDK
+      add('//connect.facebook.net/uk_UA/all.js#xfbml=1', 'facebook-jssdk');
+      // Twitter SDK
+      add('//platform.twitter.com/widgets.js');
+      fjs.parentNode.insertBefore(frag, fjs);
+    }(document, 'script'));
+  });
 })(document);
