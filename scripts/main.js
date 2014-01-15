844287b83642d33d142a69f7c0ba5b19837efd01
maidan-next <maidan-next@users.noreply.github.com>
2014-01-15 12:29:41 +0200

    add favicons

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,21 @@
 
     // Query data
     $.getJSON(DATA_URL)
+      
       .done(function(data) {
+
+        // Augment with favicons data
         for (var i=0, n=data['sections'].length; i<n; ++i) {
-          data['sections'][i]['index'] = i;
+          for (var j=0, m=data['sections'][i]['items'].length; j<m; ++j) {
+            var current = data['sections'][i]['items'][j],
+                url = current['link'],
+                image = current['image'];
+
+            if (url && !image) {
+              var a = $('<a>', {'href': url})[0];
+              current['favicon'] = a.protocol + '//' + a.hostname + '/favicon.ico';
+            }
+          }
         }
 
         console.log(data);
@@ -21,28 +33,36 @@
         $dataContainer.html(Mustache.render(template, data))
           .trigger('rendered');
       })
+
       .error(function() {
         console.error('Unable to get or parse data');
       });
 
     // Smooth scroll
-    $dataContainer.on('click', '.sidebar-link', function(e) {
+    $dataContainer
+
+      .on('click', '.sidebar-link', function(e) {
         e.preventDefault();
-        var target = $($(e.currentTarget).attr('href'));
+        var targetHash = $(e.currentTarget).attr('href'),
+            target = $(targetHash);
         $('html,body').animate({
             'scrollTop': target.offset().top
         }, 1000);
+        window.location.hash = targetHash;
         return false;
       })
+
       .on('rendered', function() {
         $body.scrollspy({
             'target': '.sidebar-wrapper'
           })
           // .scrollspy('refresh');
 
+        console.log($('header#masthead').outerHeight());
+
         $('.nav-sidebar').affix({
             'offset': {
-              'top' : 256
+              'top' : $('header#masthead').outerHeight()
             }
           });
       });
